89df70efa4bb30ce0b44f2e217413a77adaa4018
Sumeyra Ozgur <smyrozgur@gmail.com>
2021-11-11 13:16:42 -0800

    add mail config to production

--- config.py
+++ config.py
@@ -32,3 +32,9 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    MAIL_SERVER ='smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = os.environ.get('MAIL_SENDER')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
