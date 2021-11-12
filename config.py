d863a85537374a5d6816a210b264944251fb24ef
Sumeyra Ozgur <smyrozgur@gmail.com>
2021-11-11 20:12:19 -0800

    fix version bug for SQLAlchemy

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ class TestingConfig(Config):
 # create the production config
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER ='smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USERNAME = os.environ.get('MAIL_SENDER')
